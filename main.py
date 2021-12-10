fd336c4dd97d40f570c3e8f21a4002752cbd1dff
gremarsl <44293039+gremarsl@users.noreply.github.com>
2021-12-10 12:38:27 +0100

    adding switches

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import os
+import json
 from typing import Dict, List
-from requests import Response
 
-from functions_for_finnhub import get_one_absolute_indicator_from_finnhub, fundamental_data_from_finnhub, \
+import requests
+
+from functions_for_finnhub import get_one_absolute_indicator_from_finnhub, get_fundamental_data_from_finnhub, \
     get_one_relative_indicator_from_finnhub, get_one_ratio_indicator_from_finnhub
+from general_functions import write_to_file_in_json_format
 from plot_functions import stupid_plot_data_lists, stupid_plot_data_list
 from functions_for_alpha_vantage import \
     request_income_statement_from_alpha, \
@@ -15,6 +19,7 @@ from functions_for_alpha_vantage import \
     get_annual_report_alpha, \
     long_term_data_get_ebitda, \
     long_term_data_get_price_to_earning_ratio, calling_alpha_vantage_api
+
 
 # Understand JSON
 # object: {}
@@ -34,7 +39,6 @@ class NoNetMarginData(object):
 def filter_plot_data_list_per_symbol(data_list: list, data_is_from_platform: str):
     # hier:  all_data_list = [data_per_symbol_1]
 
-
     eps_ebit_per_share_plot_data = []
     if data_is_from_platform == "finnhub":
         except_grossmargin = list(filter(lambda x: (x[3] != "grossMargin"), data_list))
@@ -50,10 +54,10 @@ def filter_plot_data_list_per_symbol(data_list: list, data_is_from_platform: str
 
         stupid_plot_data_lists(eps_ebit_per_share_plot_data, data_is_from_platform)
 
-    try:
-        stupid_plot_data_lists(except_grossmargin_debt, data_is_from_platform)
-    except:
-        print("Not working to plot ratios_eps_ebit_net_margin_data data in one plot {}".format(data_list))
+        try:
+            stupid_plot_data_lists(except_grossmargin_debt, data_is_from_platform)
+        except:
+            print("Not working to plot ratios_eps_ebit_net_margin_data data in one plot {}".format(data_list))
 
     if data_is_from_platform == "alpha_vantage":
         try:
@@ -61,8 +65,7 @@ def filter_plot_data_list_per_symbol(data_list: list, data_is_from_platform: str
 
             try:
                 indicators = list(filter(
-                    lambda x: x[
-                                  3] == "grossProfit" or "ebit" or "netIncome" or "operatingIncome" or "incomeBeforeTax",
+                    lambda x: x[3] == "grossProfit" or "totalRevenue" or "ebit" or "netIncome" or "operatingIncome" or "incomeBeforeTax",
                     data_list))
                 stupid_plot_data_lists(indicators, data_is_from_platform)
             except:
@@ -79,31 +82,31 @@ def filter_plot_data_list_per_symbol(data_list: list, data_is_from_platform: str
         pass
 
 
-def get_data_from_alpha_vantage():
-    source="alpha_vantage"
-
-
-    test_symbol = "IBM"
-    symbols = ["MSFT","IBM", "AAPL"]
-    print("Income statement {}:".format(test_symbol))
+def analyse_data_from_alpha_vantage(symbols : list):
+    source = "alpha_vantage"
     print("------------------------")
-
-    indicator_absolute_income_statement = ["grossProfit", "ebit", "netIncome", "incomeBeforeTax", "operatingIncome"]
-
+    indicator_absolute_income_statement = ["grossProfit", "totalRevenue", "ebit", "netIncome", "incomeBeforeTax", "operatingIncome"]
     test = ["grossProfit", "ebit"]
 
     for s in symbols:
-        print(s)
+        income_statement_filename = "income_statement_alpha_" + s + ".json"
 
-
-        #TODO wenn ich sage jetzt hole dir die neuen, dann hole die neuen daten und überschreibe die alten Daten
-        income_statement = request_income_statement_from_alpha(s)
+        #TODO give these separate filenames and then read the json file oder enter the else
         # balance_sheets = request_balance_sheet_from_alpha(s)
         # cash_flow = request_cash_flow_from_alpha(s)
         # earnings =request_earnings_from_alpha(s)
-        overview = request_overiew_from_alpha(s)
-        #long_term_data_get_ebitda(overview,s)
-        #long_term_data_get_price_to_earning_ratio(overview,s)
+        # overview = request_overiew_from_alpha(s)
+
+        if os.path.isfile(income_statement_filename):
+            with open(income_statement_filename) as json_file:
+                income_statement = json.load(json_file)
+        else:
+            print("WARNING: alpha vantage was called but you filese are not found. Is get_alpha_data False? This should not be reached if get_alpha_data is True")
+            calling_alpha_vantage_api(symbols)
+
+
+        # long_term_data_get_ebitda(overview,s)
+        # long_term_data_get_price_to_earning_ratio(overview,s)
         annual_data_per_symbol = []
         quaterly_data_per_symbol = []
 
@@ -118,90 +121,92 @@ def get_data_from_alpha_vantage():
             except:
                 print("error in quaterly data {}".format(s))
 
-        #filter_plot_data_list_per_symbol(annual_data_per_symbol, source)
+        # filter_plot_data_list_per_symbol(annual_data_per_symbol, source)
         filter_plot_data_list_per_symbol(quaterly_data_per_symbol, source)
 
     pass
 
 
 def get_data_from_finnhub():
-    source="finnhub"
+    source = "finnhub"
     all_plot_data = []
     all_plot_data_test = []
 
-    symbol_dax_stocks = ["DAI.DE",
-                         "BAS.DE",
+    # TODO switch annual or quaterly
+    period = 'annual'
+    period = 'quarterly'
+
+    symbol_dax_stocks = ["BAS.DE",
                          "SIE.DE",
-                         "BMW.DE",
                          "BAYN.DE",
                          "IFX.DE",
                          "1COV.DE",
                          "LIN.DE",
                          "BEI.DE",
                          "HEN3.DE",
-                         "VOW3.DE"]
+                         ]
     # ALV.DE, "DBK.DE",
 
+    automotive_dax_stocks = ["DAI.DE",
+                             "BMW.DE",
+                             "VOW.DE"]
     test_symbol = ["BAS.DE"]
 
-    for s in symbol_dax_stocks:
+    for s in automotive_dax_stocks:
         data_per_symbol = []
         print(s)
 
-        fundamental_data_json = fundamental_data_from_finnhub(s)
+        fundamental_data_json = get_fundamental_data_from_finnhub(s)
 
-        indicators_per_share = ["eps", "ebitPerShare"]
         indicator_absolute_list = ["grossMargin"]  # netMargin
+        indicators_per_share = ["eps", "ebitPerShare"]
         indicators_ratio = ["cashRatio", "currentRatio"]
-        indicators_percentage=["totalDebtToEquity"]
+        indicators_percentage = ["totalDebtToEquity"]
 
         for i in indicator_absolute_list:
             try:
                 data_per_symbol.append(
-                    get_one_absolute_indicator_from_finnhub(fundamental_data_json, i, s))
+                    get_one_absolute_indicator_from_finnhub(fundamental_data_json, period, i, s))
             except:
                 print(" no {} data for {} ".format(i, s))
-
         for i in indicators_per_share:
             try:
-                data = get_one_absolute_indicator_from_finnhub(fundamental_data_json, i, s)
+                data = get_one_absolute_indicator_from_finnhub(fundamental_data_json, period, i, s)
                 data_per_symbol.append(data)
             except:
                 print("no {} data for {} ".format(i, s))
-
         for i in indicators_ratio:
             try:
                 data_per_symbol.append(
-                    get_one_ratio_indicator_from_finnhub(fundamental_data_json, i, s))
+                    get_one_ratio_indicator_from_finnhub(fundamental_data_json, period, i, s))
             except:
                 print("no {} data  for  {} ".format(i, s))
         for i in indicators_percentage:
             try:
                 data_per_symbol.append(
-                    get_one_relative_indicator_from_finnhub(fundamental_data_json, i, s))
+                    get_one_relative_indicator_from_finnhub(fundamental_data_json, period, i, s))
             except:
                 print("no {} data  for  {} ".format(i, s))
 
-        #unfiltered
-        #stupid_plot_data_lists(data_per_symbol,source)
-
-        filter_plot_data_list_per_symbol(data_per_symbol,source)
+        filter_plot_data_list_per_symbol(data_per_symbol, source)
         all_plot_data.append(data_per_symbol)
 
+
+# SWITCHES:
+analyse_finnhub_data = False
+get_alpha_data = True
+analyse_alpha_data = True
+alpha_vantage_symbols = ["AVGO"]  # "IBM", "AAPL"
+
 if __name__ == '__main__':
-    get_data_from_finnhub()
 
-    get_data_from_alpha_vantage()
+    if analyse_finnhub_data:
+        get_data_from_finnhub()
 
-    symbols = ["MSFT", "IBM", "AAPL"]
-    calling_alpha_vantage_api(symbols)
+    if get_alpha_data:
+        calling_alpha_vantage_api(alpha_vantage_symbols)
 
-    # TODO use balance sheet -> goodwill unter anderem
+    if analyse_alpha_data:
+        analyse_data_from_alpha_vantage(alpha_vantage_symbols)
 
     # TODO workflow erstellen wenn neues File auf neue Faktoren untersucht werden muss
-
-    # TODO How TO HANDLE exception
-
-    # TODO LONGTERM DATA checken
-
-    #TODO log on log off
