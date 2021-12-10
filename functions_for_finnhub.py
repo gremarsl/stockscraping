a22983595a7050bda2aad831fe569f820274b7d9
gremarsl <44293039+gremarsl@users.noreply.github.com>
2021-12-10 12:38:09 +0100

    add period as argument

--- functions_for_finnhub.py
+++ functions_for_finnhub.py
@@ -8,13 +8,13 @@ from general_functions import write_to_file_in_json_format, reverse_lists
 api_url = "https://finnhub.io/api/v1/stock/metric?"
 
 
-def fundamental_data_from_finnhub(symbol: str) -> Response:
+def get_fundamental_data_from_finnhub(symbol: str) -> Response:
     data = {
         "symbol": symbol,
         "metric": "all",
         "token": api_key}
 
-    filename = "fundamental_data_" + symbol + ".json"
+    filename = "fundamental_data_finnhub_" + symbol + ".json"
 
     if os.path.isfile(filename):
         with open(filename) as json_file:
@@ -23,14 +23,16 @@ def fundamental_data_from_finnhub(symbol: str) -> Response:
     else:
         fundamental_data_response = requests.get(api_url, data)
         fundamental_data_response_json: requests.models.Response = fundamental_data_response.json()  # maybe redundant
+        # 'https://finnhub.io/api/v1/stock/metric?symbol=DAI.DE&metric=all&token=buk3id748v6r2017iuog'
 
         write_to_file_in_json_format(fundamental_data_response_json, filename)
 
     return fundamental_data_response_json
 
 
-def get_one_absolute_indicator_from_finnhub(data_json: dict, indicator: str, symbol: str) -> list:
-    data = data_json['series']['annual'][indicator]
+def get_one_absolute_indicator_from_finnhub(data_json: dict,period :str, indicator: str, symbol: str) -> list:
+
+    data = data_json['series'][period][indicator]
     time_points = []
     value_points = []
     for i in data:
@@ -45,8 +47,8 @@ def get_one_absolute_indicator_from_finnhub(data_json: dict, indicator: str, sym
     return data_list
 
 
-def get_one_ratio_indicator_from_finnhub(data_json: dict, indicator: str, symbol: str) -> list:
-    data = data_json['series']['annual'][indicator]
+def get_one_ratio_indicator_from_finnhub(data_json: dict,period:str, indicator: str, symbol: str) -> list:
+    data = data_json['series'][period][indicator]
     time_points = []
     value_points = []
     for i in data:
@@ -61,8 +63,8 @@ def get_one_ratio_indicator_from_finnhub(data_json: dict, indicator: str, symbol
     return data_list
 
 
-def get_one_relative_indicator_from_finnhub(data_json: dict, indicator: str, symbol: str) -> list:
-    data = data_json['series']['annual'][indicator]
+def get_one_relative_indicator_from_finnhub(data_json: dict,period:str, indicator: str, symbol: str) -> list:
+    data = data_json['series'][period][indicator]
     time_points = []
     value_points = []
     for i in data:
