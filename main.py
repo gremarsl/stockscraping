e5a2c1cd8229d6cab0e1360dc04a96716bcd0817
gremarsl <44293039+gremarsl@users.noreply.github.com>
2022-01-27 22:45:53 +0100

    add switch for my own json

--- main.py
+++ main.py
@@ -10,15 +10,6 @@ from functions_for_alpha_vantage import calling_alpha_vantage_api
 # array : []
 # key-value pair is object data
 
-# SWITCHES:
-build_own_json = 1
-analyse_my_json_data =1
-get_finnhub_data = 0
-analyse_finnhub_data = 0
-get_alpha_data = 0
-analyse_alpha_data = 1
-analyse_alpha_data_compare_companies = 0
-
 automotive_finnhub = ["DAI.DE", "BMW.DE", "VOW.DE", "PAH3.DE"]
 chemicals_finnhub = ["BAS.DE", "BAYN.DE", "LIN.DE", "HEN3.DE", "1COV.DE"]
 industry_finnhub = ["SIE.DE"]
@@ -29,6 +20,7 @@ semiconductor = ["INTC", "AMD", "ASML", "NVDA", "KLAC", "UMC", "TSM", "QCOM", "S
 semiconductor_nasdaq_alpha = ["IBM", "MSFT", "AAPL", "AMD", "ASML", "NVDA", "KLAC", "TEAM", "UMC", "TSM"]
 semiconductor_nasdaq_alpha2 = ["ZS", "AMBA", "QCOM", "AVGO", "STM", "MRVL", "VSH", "SNPS", "VLDRW", "MU", "ADI"]
 
+automotive_alpha = ["TSLA"]
 network_alpha = ["NOK", "VZ", "T"]
 consumer_alpha = ["PRG", "KO", "MCD", "NKE", "DIS"]
 mobility_alpha = ["BA"]
@@ -38,16 +30,21 @@ finance_alpha = ["V"]
 get_finnhub_symbol = ["IFX.DE"]
 analyse_finnhub_symbol = automotive_finnhub
 
-get_alpha_vantage_symbol_data = pharma_companies_alpha
-analyse_alpha_vantage_symbol_data = pharma_companies_alpha
+get_alpha_vantage_symbol_data = automotive_alpha
+analyse_alpha_vantage_symbol_data = automotive_alpha
 
-my_json_symbol = ["JNJ"]
+my_json_symbol = ["TSLA"]
+
+# SWITCHES:
+build_own_json = 0
+analyse_my_json_data =1
+get_finnhub_data = 0
+analyse_finnhub_data = 0
+get_alpha_data = 0
+analyse_alpha_data = 0
+analyse_alpha_data_compare_companies = 0
+
 if __name__ == '__main__':
-    if build_own_json ==1:
-        build_own_json_file(my_json_symbol)
-
-    if analyse_my_json_data == 1:
-        analyse_data_from_local_json_file(my_json_symbol)
 
     if get_finnhub_data == 1:
         calling_finnhub_api(get_finnhub_symbol)
@@ -60,3 +57,9 @@ if __name__ == '__main__':
 
     if analyse_alpha_data == 1 or analyse_alpha_data_compare_companies == 1:
         analyse_data_from_alpha_vantage(analyse_alpha_vantage_symbol_data, analyse_alpha_data_compare_companies)
+
+    if build_own_json ==1:
+        build_own_json_file(my_json_symbol)
+
+    if analyse_my_json_data == 1:
+        analyse_data_from_local_json_file(my_json_symbol)
