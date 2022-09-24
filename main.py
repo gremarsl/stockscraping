2e1372ed92cff866a3c9eb26a6226c5321c4a335
gremarsl <44293039+gremarsl@users.noreply.github.com>
2022-09-24 23:07:27 +0200

    merge two json files to one. This facilites the analysis later on, since only one file needs to be considered

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from build_own_json import build_own_json_file
 from functions_for_finnhub import calling_finnhub_api
 from functions_for_alpha_vantage import calling_alpha_vantage_api
 from functions_for_yahoo import calculate_sp_500_to_gdp_usa, calculate_dax_to_gdp_germany, get_yahoo_data
-from general_functions import read_data_from_file, append_key_value_to_object, append_object_to_json_array
+from general_functions import read_data_from_file, append_key_value_to_object, append_object_to_json_array, \
+    write_to_file_in_json_format
 
 
 def finnhub_analysis():
@@ -47,43 +48,42 @@ def own_json_analysis():
     print("end own json analysis  ...")
 
 
-def copy_and_rename_file(src_file,dest_file):
+def copy_and_rename_file(src_file, dest_file):
     shutil.copy(src_file, dest_file)
 
 
-
-
-
-
 def merge_two_json_files():
-    files = ['yahoo_quarterly_financials_MSFT.json', 'yahoo_quarterly_balance_sheet_MSFT.json']
-    print(files)
+    filenames = ['yahoo_quarterly_financials_MSFT.json', 'yahoo_quarterly_balance_sheet_MSFT.json']
+    print(filenames)
 
     dest_file = "yahoo_total_data_MSFT.json"
 
-    copy_and_rename_file(files[0],dest_file)
+    copy_and_rename_file(filenames[0], dest_file)
 
     base_file = read_data_from_file(dest_file)
-    merge_file = read_data_from_file(files[1])
+    merge_file = read_data_from_file(filenames[1])
 
-    '''
+
+    #TODO improve error logging
     for quarter in merge_file["quarterlyReports"]:
         date = quarter["fiscalDateEnding"]
 
-        #base_quarter is an object
+        # base_quarter is an object
         for base_quarter in base_file["quarterlyReports"]:
 
             base_date = base_quarter["fiscalDateEnding"]
 
             if date == base_date:
-                print("Iterate over key value pairs. Add to object if not already there. ")
+                for key_value_pair in quarter.items():
+                    print(key_value_pair)
+                    print(key_value_pair[0])
+                    print(key_value_pair[1])
 
-            else:
-                print("Quartals are not equal")
-                print(date + " and " +base_date + " are not equal ")
+                    #Iterate over key value pairs. Add to object if not already there.
+                    if key_value_pair not in base_quarter:
+                        append_key_value_to_object(base_quarter,key_value_pair[0],key_value_pair[1])
 
-                append_object_to_json_array(merge_file["quarterlyReports"],base_file["quarterlyReports"])
-    '''
+    write_to_file_in_json_format(base_file,dest_file + "_2")
 
 def yahoo_data_analysis():
     print("start yahoo finance analysis ...")
@@ -94,7 +94,7 @@ def yahoo_data_analysis():
     merge_two_json_files()
 
     if global_vars.analyze_yahoo_data == 1:
-        analyze_data_from_local_json_file(global_vars.yahoo_symbols,global_vars.analyze_yahoo_compare_companies)
+        analyze_data_from_local_json_file(global_vars.yahoo_symbols, global_vars.analyze_yahoo_compare_companies)
     print("end yahoo finance analysis ...")
 
 
