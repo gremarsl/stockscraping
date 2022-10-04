ab80b1649904ee73411f25ab670a5b9cb314c776
gremarsl <44293039+gremarsl@users.noreply.github.com>
2022-10-04 23:02:31 +0200

    added index calc again

--- global_vars.py
+++ global_vars.py
@@ -14,7 +14,7 @@ import os
 '''
 # START USER CONFIG SWITCHES
 # set value to 1, to enable an analysis and calculation. set value to 0 to disable it.
-GET_YAHOO_DATA            = 1
+GET_YAHOO_DATA            = 0
 ANALYZE_YAHOO_DATA        = 0
 COMPARE_YAHOO_COMPANIES   = 1
 
@@ -45,3 +45,11 @@ directory_of_execution  = os.getcwd()
 filepath_my_json        = directory_of_execution + "\\reports_json\\"
 filepath_yahoo          = directory_of_execution + "\\yahoo_info\\"
 # END FILEPATH PARAMETER
+
+
+# START PARAMETERS FOR INDEX CALCULATION
+SP_500_DIVISOR = 8451.33
+GDP_USA = 19731.10  # billion
+GDP_GERMANY = 3806  # billion
+DAX = ["SY1.DE", "1COV.DE", "SHL.DE", "DPW.DE", "CON.DE", "PUM.DE", "AIR.DE", "BEI.DE", "DDAIF", "HEN.DE", "IFX.DE", "ADS.DE", "HNR1.DE", "SIE.DE", "FME.DE", "VOW3.DE", "LIN.DE", "DBK.DE", "BAS.DE", "MRK.DE", "DB1.DE", "ZAL.DE", "FRE.DE", "RWE.DE", "DTE.DE", "BAYN.DE", "BMW.DE", "MTX.DE", "ALV.DE", "HEI.DE", "HFG.DE", "DTG.DE", "BNR.DE", "CON.DE", "EOAN.DE", "QGEN", "PAH3.DE", "SAP.DE", "VNA.DE", "ZAL.DE"]
+# END PARAMETERS FOR INDEX CALCULATION
