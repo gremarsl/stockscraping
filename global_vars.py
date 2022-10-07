38d5ef674ce430ac44d1f9071210fb3a100afd03
gremarsl <44293039+gremarsl@users.noreply.github.com>
2022-10-07 14:52:07 +0200

    improved documentation

--- global_vars.py
+++ global_vars.py
@@ -6,11 +6,18 @@ import os
 # **********************************************************************************************************************
 # Constants
 # **********************************************************************************************************************
+
 '''
-@param GET_YAHOO_DATA                     Requests / Gets data from yahoo finance for the entered companies. 
-                                          If enabled / set to 1.
+@param symbol_list          Companies to analyze
+'''
+SYMBOL_LIST = ["MSFT", "AAPL"]
+
+
+'''
+@param GET_YAHOO_DATA                     Requests data from yahoo finance for the entered companies. 
+                                          If enabled, data is requested for each company symbol with the list
 @param ANALYZE_YAHOO_DATA                 Evaluates the available data from yahoo finance for the 
-                                          entered companies. If enabled / set to 1 every company get evaluated and 
+                                          entered companies. If enabled every company get evaluated and 
                                           can be interpreted with the help of graphics showing financial indicators 
                                           separately.
 @param COMPARE_YAHOO_COMPANIES            Evaluates the available data from alpha vantage. 
@@ -18,8 +25,7 @@ import os
                                           together in one graphic for easier comparison.
 '''
 
-# set value to 1, to enable an analysis and calculation. set value to 0 to disable it.
-GET_YAHOO_DATA = 1
+GET_YAHOO_DATA = 0
 ANALYZE_YAHOO_DATA = 0
 COMPARE_YAHOO_COMPANIES = 1
 
@@ -31,19 +37,28 @@ CALC_DAX = 0
 CALC_SP500 = 0
 
 '''
-@param symbol_list          Companies to analyze
-'''
-SYMBOL_LIST = ["MSFT", "AAPL"]
+# set value to 1, to enable an analysis and calculation. set value to 0 to disable it.
 
-# TODO
+@param ANALYZE_YAHOO_ABS                If enabled, script analyzes yahoo absolute indicators 
+                                        from list ABS_INDICATOR_LIST and plots the data.
+                                        e.g. TotalRevenue,NetIncome,TotalAssets
+@param ANALYZE_YAHOO_REL                If enabled, script analyzes yahoo relative indicators 
+                                        from list REL_INDICATORS_LIST and plots the data.
+                                        e.g. TotalRevenue/TotalAssets
+@param ANALYZE_YAHOO_REL_LIVE           If enabled, script analyzes live yahoo relative indicator 
+                                        from list REL_LIVE_INDICATOR_LIST and plots the data.
+                                        If enabled the entered companies are evaluated and shown 
+                                        together in one graphic for easier comparison.
+                                        e.g. TotalRevenue/live_marketCap
+'''
 ANALYZE_YAHOO_ABS = 0
 ANALYZE_YAHOO_REL = 0
 ANALYZE_YAHOO_REL_LIVE = 1
 
-# TODO
 ABS_INDICATOR_LIST = ["TotalRevenue", "NetIncome"]
+
 REL_INDICATORS_LIST = ["NetIncome_to_TotalRevenue", "ResearchDevelopment_to_TotalRevenue", "TotalLiab_to_TotalAssets",
-                       "TotalCurrentLiab_to_TotalCurrentAssets"]  # ,
+                       "TotalCurrentLiab_to_TotalCurrentAssets"]
 
 REL_LIVE_INDICATOR_LIST = ["marketCap_to_TotalRevenue", "marketCap_to_NetIncome", "marketCap_to_TotalAssets"]
 
