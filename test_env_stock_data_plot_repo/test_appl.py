afde53ed56e28c49e0c3fa54242e78462aba66d7
gremarsl <44293039+gremarsl@users.noreply.github.com>
2022-11-03 20:34:40 +0100

    added pdf_merger

--- test_env_stock_data_plot_repo/test_appl.py
+++ test_env_stock_data_plot_repo/test_appl.py
@@ -1,40 +1,39 @@
 import os
 
+from general_functions import pdf_merger
 from structure_data import stockDataPlots
 
 from plotter import Plotter
 from processor import Processor
 
 
-
-
-
 def main():
     # TODO falsche fehlermeldung
     # New Folder for ticker GOOGL has been created at C:\Users\marce\PycharmProjects\stockscraperFinnhub\AAPL\
-    company_ticker = "GOOGL"
-    data = stockDataPlots(company_ticker)
 
-    print(data)
+    # TODO live market cap
+    symbol_list = ["NVDA"]
+    data = stockDataPlots(symbol_list[0])
     data.getCurrentYahooData()
 
-    folder_path = os.getcwd() + f"\\{company_ticker}\\"
+    folder_path = os.getcwd() + f"\\{symbol_list[0]}\\"
 
     # TODO append works already?
     data.updateLocalData(folder_path)
-
-    symbol_list = ["GOOGL"]
 
     file_list = [
         os.getcwd() + f"\\{symbol_list[0]}\\{symbol_list[0]}\\" + "balanceSheetQuarterly.csv",
         os.getcwd() + f"\\{symbol_list[0]}\\{symbol_list[0]}\\" + "cashflowQuarterly.csv",
         os.getcwd() + f"\\{symbol_list[0]}\\{symbol_list[0]}\\" + "incomeStatementQuarterly.csv"]
 
-    destination_file_path = Processor.merge_csv_files(file_list, symbol_list)
+    Processor.merge_csv_files(file_list, symbol_list)
 
     plotter = Plotter(symbol_list[0])
 
     plotter.plot_all()
+
+    # TODO this is a possible method
+    pdf_merger(os.getcwd()+ f"\\{symbol_list[0]}")
 
 
 # ----------------------------------------------------------------------------------------------------------------------
